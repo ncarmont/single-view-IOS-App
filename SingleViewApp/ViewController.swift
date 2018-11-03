33ab69deb6c1ef94f0b2ae3f7743bd0377b4bedd
Nicolas Carmont <nickcarmont8@gmail.com>
2018-11-03 08:52:32 -0700

    push button counter func

--- SingleViewApp/ViewController.swift
+++ SingleViewApp/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var lblMessage: UILabel!
+    
+    var clickCount: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        lblMessage.text = "Button clicked 0 times"
+    }
+    
+    @IBAction func buttonTouched(sender: UIButton){
+        clickCount += 1;
+        lblMessage.text = "Button clicked \(clickCount) times"
     }
 
 
