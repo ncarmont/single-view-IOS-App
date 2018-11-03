2a2c9882e6f1356e339a6c2dac5efac8a0236c44
Nicolas Carmont <nickcarmont8@gmail.com>
2018-11-03 08:09:06 -0700

    first commit

---
+++ SingleViewApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SingleViewApp
+//
+//  Created by Computer Science on 03/11/2018.
+//  Copyright © 2018 Nicolas Carmont. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
